3a0b5c304a7bbf29c51a7dda35eab4755fa51693
me_nyong <gjalsdud1030@naver.com>
2026-05-27 17:06:04 +0900

    [FE/feat#24] /api로 시작하는 요청은 8089포트로 전달(vite.config.js)

--- frontend/emergency-road-web/vite.config.js
+++ frontend/emergency-road-web/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // '/api'로 시작하는 요청을 백엔드 서버(8089)로 전달
+      '/api': {
+        target: 'http://localhost:8089',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
