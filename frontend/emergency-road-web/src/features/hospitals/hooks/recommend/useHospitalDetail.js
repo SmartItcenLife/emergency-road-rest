77b751f6ecda1c0263ae519ea701a678e3480b43
me_nyong <gjalsdud1030@naver.com>
2026-05-27 17:04:41 +0900

    [FE/feat#24] 상세보기 커스텀 훅 구현 (useHospitalDetail.js)

---
+++ frontend/emergency-road-web/src/features/hospitals/hooks/recommend/useHospitalDetail.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import { getHospitalDetail } from '../api/hospitalDetail';
+
+export const useHospitalDetail = (hpid) => {
+    const [detailData, setDetailData] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const toggleDetail = async () => {
+        // 펼치려는 상황이고, 아직 데이터를 안 받아왔을 때만 API 호출
+        if (!isExpanded && !detailData) {
+            setLoading(true);
+            try {
+                const data = await getHospitalDetail(hpid);
+                setDetailData(data);
+            } catch (error) {
+                console.error("상세 정보 로드 실패", error);
+            } finally {
+                setLoading(false);
+            }
+        }
+        setIsExpanded(!isExpanded);
+    };
+
+    return { detailData, loading, isExpanded, toggleDetail };
+};
