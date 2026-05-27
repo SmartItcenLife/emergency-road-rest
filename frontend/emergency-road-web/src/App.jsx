dbaa76dcb93160d908ae0d809c503d1fcad253e5
rehayoung <leehayoung1114@gmail.com>
2026-05-27 14:13:17 +0900

    [feat#17] Kakao Map 기반 General 병원 마커 표시 및 지도 범위 기반 조회 API 구현 - component,page 기본 세팅 중간 커밋

--- frontend/emergency-road-web/src/App.jsx
+++ frontend/emergency-road-web/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './shared/components/layout/Header';
 import Home from './pages/Home';
 import AdminDashboardPage from './pages/admin/AdminDashboardPage'
 import AdminUserListPage from './pages/admin/AdminUserListPage.jsx'
+import MapPage from './pages/map/MapPage';
 
 // 헤더를 감싸는 래퍼 컴포넌트
 const Layout = () => {
@@ -25,7 +26,7 @@ const Layout = () => {
         <Route path="/" element={<Home />} />
         <Route path="/admin" element={<AdminDashboardPage />} />
         <Route path="/admin/users" element={<AdminUserListPage />} />
-    
+        <Route path="/map" element={<MapPage />} />
         {/* <Route path="/recommend" element={<Recommend />} />
         <Route path="/community" element={<Community />} /> */}
       </Routes>
---
+++ frontend/emergency-road-web/src/features/map/components/KakaoMap.jsx
@@ -0,0 +1,10 @@
+function KakaoMap() {
+  return (
+    <div className="kakao-map">
+      {/* 카카오맵이 여기에 렌더링됩니다. */}
+      지도 영역
+    </div>
+  );
+}
+
+export default KakaoMap;
