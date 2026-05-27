61b8d85bad0adbfe1929263c6f84a9ba4243803c
7888mk0915@gmail.com <7888mk0915@gmail.com>
2026-05-27 22:56:28 +0900

    feat/admin App에 신고 페이지 링크 추가

--- frontend/emergency-road-web/src/App.jsx
+++ frontend/emergency-road-web/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import AdminDashboardPage from './pages/admin/AdminDashboardPage'
 import AdminUserListPage from './pages/admin/AdminUserListPage.jsx'
 import AdminPostListPage from './pages/admin/AdminPostListPage.jsx';
+import AdminReportListPage from './pages/admin/AdminReportListPage.jsx';
 import MapPage from './pages/map/MapPage';
 
 // 헤더를 감싸는 래퍼 컴포넌트
@@ -29,6 +30,7 @@ const Layout = () => {
         <Route path="/admin/users" element={<AdminUserListPage />} />
         <Route path="/admin/posts" element={<AdminPostListPage />} />
         <Route path="/map" element={<MapPage />} />
+        <Route path="/admin/reports" element={<AdminReportListPage/>} />
         {/* <Route path="/recommend" element={<Recommend />} />
         <Route path="/community" element={<Community />} /> */}
       </Routes>
