3f4be9af95f6f3618080483f033e8381ad2464d0
me_nyong <gjalsdud1030@naver.com>
2026-05-28 16:19:25 +0900

    [FE/feat#35] 전체보기할 때 랭킹 보여주지 않기, 옵션이 거리일 때만 거리 및 소요시간 보여주기

--- frontend/emergency-road-web/src/features/hospitals/components/RecommendedHospitalSection.jsx
+++ frontend/emergency-road-web/src/features/hospitals/components/RecommendedHospitalSection.jsx
@@ -11,8 +11,8 @@ const RecommendedHospitalSection = ({ category, lat, lon }) => {
 
     if (loading) return <div>병원 목록을 불러오는 중입니다...</div>;
     if (error) return <div>오류가 발생했습니다: {error.message}</div>;
-
-    return <HospitalList hospitals={data} userLat={lat} userLon={lon} config ={config} category = {category} />;
+    // 추천 리스트일 때는 showRanking true, 거리 소요시간 보여주기 
+    return <HospitalList hospitals={data} userLat={lat} userLon={lon} config ={config} category = {category}  showRanking={true} showDistance={true} />;
 };
 
 export default RecommendedHospitalSection;
