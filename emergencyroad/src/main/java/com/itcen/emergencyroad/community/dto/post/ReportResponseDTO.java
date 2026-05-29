790837c33b3c0d59ffdd46b1431c6899200865ff
7888mk0915@gmail.com <7888mk0915@gmail.com>
2026-05-29 18:18:39 +0900

    feat/admin 관리자 신고 목록 응답 DTO 정리

--- emergencyroad/src/main/java/com/itcen/emergencyroad/community/dto/post/ReportResponseDTO.java
+++ emergencyroad/src/main/java/com/itcen/emergencyroad/community/dto/post/ReportResponseDTO.java
@@ -1,4 +1,4 @@
-package com.itcen.emergencyroad.community.dto;
+package com.itcen.emergencyroad.community.dto.post;
 
 import com.itcen.emergencyroad.community.entity.Report;
 import com.itcen.emergencyroad.community.enums.ReportTargetType;
