92b1199883148aba46032f24ab5a435820947b32
7888mk0915@gmail.com <7888mk0915@gmail.com>
2026-05-29 11:42:30 +0900

    feat/admin 헤더 파일 위치 옮김. component->page

---
+++ frontend/emergency-road-web/src/pages/admin/AdminHeader.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './AdminHeader.css';
+import logo from '../../assets/logo.png'
+const AdminHeader = () => {
+  return (
+    <header className="admin-header">
+
+      <div className="admin-logo">
+            <a className="admin-logo-link" href="/admin">
+              <img className="admin-logo-img" src = {logo} width={50}/>        
+              <span>관리자 페이지</span>
+            </a>
+      </div>
+
+      <nav className="admin-menu">
+        <a href="/admin/users">회원 관리</a>
+        <a href="/admin/reports">신고 관리</a>
+        <a href="/admin/posts">커뮤니티 관리</a>
+      </nav>
+
+      <button>로그아웃</button>
+
+    </header>
+  );
+};
+
+export default AdminHeader;
