0defad20945ec4b4805c5ab23b12194fe243db9e
me_nyong <gjalsdud1030@naver.com>
2026-06-01 09:37:27 +0900

    [FE/chore] vite.config.js 백엔드 포트번호 8088 변경

--- frontend/emergency-road-web/vite.config.js
+++ frontend/emergency-road-web/vite.config.js
@@ -8,9 +8,9 @@ export default defineConfig({
     host: "0.0.0.0",
     port: 5713,
     proxy: {
-      // '/api'로 시작하는 요청을 백엔드 서버(8089)로 전달
+      // '/api'로 시작하는 요청을 백엔드 서버(8088)로 전달
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:8088',
         // target: 'http://localhost:8080',
         changeOrigin: true,
         secure: false,
